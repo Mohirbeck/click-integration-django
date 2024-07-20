63c2f81d1d1b1cbac78777b49e258acc99832497
Mohirbek <mustaqimovmohir@gmail.com>
2024-07-20 17:04:20 +0500

    Update forms.py

--- click/forms.py
+++ click/forms.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from requests.exceptions import HTTPError
 
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django import forms
 from payments.forms import CreditCardPaymentFormWithName, PaymentForm
 from payments.fields import CreditCardNumberField
